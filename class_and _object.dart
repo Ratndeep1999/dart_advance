129680027d709f94d2aa2c739be900a82008b7ae
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-05 21:03:31 +0530

    Class add

--- class_and _object.dart
+++ class_and _object.dart
@@ -2,6 +2,15 @@
 
 // Class and Object
 
+// Class :
+// class is a blueprint of any object
+// class is written outside the main()
+// it follow PascaleCase naming conversion
+// class model (body) has properties, methods, and getter and setter
+// class have 4 main pillars concept
+
+
 void main(){
+
 
 }
