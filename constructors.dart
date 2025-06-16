7aa07ef191d10062959295150abe9cbb67b5808b
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-16 15:45:03 +0530

    constructors.dart : basic e.g before learn constructor, constructor?,

--- constructors.dart
+++ constructors.dart
@@ -25,12 +25,22 @@ class Person{
 // called automatically when an object is created
 // can be use to set initial values for object properties
 
+// points to be note :
+// constructor name should be same as class name
+// constructor doesn't have any return type
+
+
+// Note : whenever object is created, the constructor is called automatically,
+// it is use to initialize values when an object is created
+
+
 
 void main(){
-
   // object of person class
   Person person = Person() ;
 
   person.getData('Ratndeep', 26) ;  // positions parameters
   person.printDetails() ;
+
+
 }
