9aa31f92318583dfbf2688e41ad3ae866f21177e
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-16 18:36:29 +0530

    constructors_types.dart : 1.Default constructor, 2.Parameterized constructor, 3.Named constructor, 4.Const constructor

--- constructors_types.dart
+++ constructors_types.dart
@@ -1,4 +1,34 @@
 
 // Constructors and their types
 
-// Default constructors
+// 1.Default constructors
+// constructor which is automatically created by dart compiler
+// if I will not create then default constructor will call automatically
+// default constructor has no parameters
+// declare using the class name followed by parenthesis
+
+// e.g of person class
+class Person{
+  String? name ;
+  int? age ;
+  double? height ;
+
+  Person(){
+    print('Default constructor call\'ed');
+    age = 26 ;
+  }
+}
+
+
+void main(){
+  // object of person class
+  Person person = Person();
+  person.name = 'Sonal' ;
+  person.height = 5.5 ;
+  print('Name : ${person.name}');
+  print('Age : ${person.age}');
+  print('Height : ${person.height}');
+
+
+
+}
