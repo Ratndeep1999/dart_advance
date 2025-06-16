6c20a5f744c590f4752fbbbdeaf195108564bc56
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-16 18:49:24 +0530

    constructors_types.dart : 1.Default constructor, 2.Parameterized constructor and parameterized constructor with Name parameters, 3.Named constructor, 4.Const constructor

--- constructors_types.dart
+++ constructors_types.dart
@@ -1,7 +1,7 @@
 
 // Constructors and their types
 
-// 1.Default constructors
+// 1.Default constructors :
 // constructor which is automatically created by dart compiler
 // if I will not create then default constructor will call automatically
 // default constructor has no parameters
@@ -13,9 +13,32 @@ class Person{
   int? age ;
   double? height ;
 
+  // default constructor
   Person(){
     print('Default constructor call\'ed');
     age = 26 ;
+  }
+}
+
+
+// 2.Parameterized constructor :
+// it is use to initialized instance variable of class
+// this is constructor that takes parameters
+// it is use to pass the values to constructor at the time of object creation
+
+// e.g of buss class
+class Bus{
+  String? color ;
+  int? numOfSeats ;
+  bool? isDriverless ;
+
+  // Parameterized constructor
+  Bus(this.color, this.numOfSeats, this.isDriverless);
+
+  void printDetails(){
+    print('Bus color : ${this.color}');
+    print('Number of seats in Bus : ${this.numOfSeats}');
+    print('Is Bus Driverless : ${this.isDriverless}');
   }
 }
 
@@ -28,7 +51,11 @@ void main(){
   print('Name : ${person.name}');
   print('Age : ${person.age}');
   print('Height : ${person.height}');
+  print('\n');
 
 
+  // object of Bus class
+  Bus bus = Bus('Black', 56, false);
+  bus.printDetails();
 
 }
