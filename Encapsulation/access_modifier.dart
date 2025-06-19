37acc287fb50dbe0a4aa8d10018c88083a3d605b
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-19 11:13:45 +0530

    access_modifier.dart :

--- Encapsulation/access_modifier.dart
+++ Encapsulation/access_modifier.dart
@@ -2,9 +2,10 @@
 
 /// Access Modifier : Public and Private
 
-/// Public :
+/// Public ( properties nnd methods) : members are accessible from any part of the program
 
-/// Private :
+/// Private ( properties nnd methods) : members are accessible only within same file, where they are declared
+/// private members declared using _ (underscore)
 
 
 
