77ec51d4b50018ce070afb68107b3f5406a3264e
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-19 11:18:01 +0530

    access_modifier.dart : Public and Private ( properties and methods ),

--- Access Modifiers/access_modifier.dart
+++ Access Modifiers/access_modifier.dart
@@ -3,10 +3,11 @@
 /// Access Modifier : Public and Private
 
 /// Public ( properties nnd methods) : members are accessible from any part of the program
+// e,g  int age
 
 /// Private ( properties nnd methods) : members are accessible only within same file, where they are declared
 /// private members declared using _ (underscore)
-
+// e.g   int _age
 
 
 
