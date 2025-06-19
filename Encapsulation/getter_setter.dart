523fca9a76c6731b00db9123d20f698a5466584b
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-19 22:15:43 +0530

    getter_setter.dart : getter and setter

--- Encapsulation/getter_setter.dart
+++ Encapsulation/getter_setter.dart
@@ -1,3 +1,13 @@
 
 
-// getter and setter
+/// Getter and Setter : both are use to access and update value of private members of class.
+// both are special methods that provide controlled access on object's properties
+// both enabled Encapsulation of dart
+// Both allow to define how to read( getter ) and modified( setter ) a properties
+
+/// Getter ( Read ) : method are use to access value of private property.
+
+
+
+
+/// Setter ( modify ) : method are use to update the value of private property.
