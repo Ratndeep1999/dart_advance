fe11d72807850d65d65755fa2b2b42391eb9564b
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-19 10:18:16 +0530

    function_overloading.dart : Function Overloading

---
+++ function_overloading.dart
@@ -0,0 +1,23 @@
+
+// Function Overloading :
+// it is basically a concept
+// that allow to define multiple function have the same name
+// just by changing the parameters list of function
+
+class FuncOver{
+
+  // method 1
+  void functOver(){}
+
+  // method 2
+  // void functOver(int? x){}   // this method with same name it give compile time error
+}
+
+
+class Test{
+
+  // void Test(){   // it give compile time error bec. class name and method name is same
+  //
+  // }
+
+}
