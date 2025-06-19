9f5c79dbf1b3f20e003d3892a0f3ccfa8398b37d
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-19 11:05:02 +0530

    hashcode.dart : e.g of hashcode, difference between object and reference, every Class inherit from Object class

--- hashcode.dart
+++ hashcode.dart
@@ -4,9 +4,15 @@
 /// it is an address of an object in memory,
 /// which help's to identify object uniquely
 
-
 // e.g of unique hashcode of objects
 class Test { }
+
+
+/// Reference : difference between object and reference
+/// variable that holds the address or hashcode of that object is known as reference
+/// Object is basically instance of class that will be present inside memory
+// e.g of reference
+class Demo { }
 
 
 /// Every Class in dart inherit from Object class
@@ -28,7 +34,13 @@ void main(){
 
   print('\n');
 
+  // object of Demo class
+  Demo d1 = Demo();
+  Demo d2 = d1 ;  // here d1 is assign to d2 which reference same object
+  print('d1 hashcode : ${d1.hashCode}');  // both hashcode will same
+  print('d2 hashcode : ${d2.hashCode}');
 
+  print('\n');
 
   // object of Xyz class
   Xyz xyz = Xyz();
