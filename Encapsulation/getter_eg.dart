64bc308dcd543551524cc3bf1a3ba20558c43bd8
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-20 17:25:36 +0530

    getter_eg.dart : e.g of getters

--- Encapsulation/getter_eg.dart
+++ Encapsulation/getter_eg.dart
@@ -3,6 +3,8 @@
 // getter
 
 // class Person
+import 'dart:math';
+
 class Person {
   String firstName ;
   String lastName ;
@@ -17,6 +19,33 @@ class Person {
 
 }
 
+
+// class of notebook
+class NoteBook {
+  String _name ;
+  dynamic _prize ;
+
+  NoteBook(this._name, this._prize);
+
+  // getter for name
+  String get name {
+    if ( _name == '') {
+      _name = " Unknown ";
+    }
+    return this._name ;
+  }
+
+
+  // getter for prize
+  dynamic get price {
+    if ( _prize == 0 ) {
+      _prize = " Not for sale "  ;
+    }
+    return this._prize ;
+  }
+
+}
+
 void main() {
   // object of person
   Person person = Person('Ratndeep', 'Chandankhede');
@@ -24,5 +53,19 @@ void main() {
 
   print("\n");
 
-  //
+  // object of Notebook
+  NoteBook noteBook1 = NoteBook('Sonali', 100);
+  print('Name : ${noteBook1.name}');
+  print('Price : ${noteBook1.price}');
+
+
+  // object of Notebook
+  NoteBook noteBook2 = NoteBook('', 0);
+  print('Name : ${noteBook2.name}');
+  print('Price : ${noteBook2.price}');
+
+  print("\n");
+
+
+
 }
