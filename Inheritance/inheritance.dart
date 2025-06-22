386061e6eb025f21714d4762ac7df8bc2e9db97f
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-22 22:49:56 +0530

    inheritance.dart : Inheritance,

--- Inheritance/inheritance.dart
+++ Inheritance/inheritance.dart
@@ -2,8 +2,8 @@
 /// Inheritance
 
 // It is basically concept of inherit properties from Parent clas to Child class.
-// In real world, when Child gets property or something from Parent that is concept of inheritance
-
+// In real world, when Child gets property or something from Parent that is concept of inheritance.
+//
 
 
 
