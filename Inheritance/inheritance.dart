7e3027a742d6ef3532e1a9b60a95105311efaa4c
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-23 21:38:49 +0530

    inheritance.dart : defination,

--- Inheritance/inheritance.dart
+++ Inheritance/inheritance.dart
@@ -2,8 +2,11 @@
 /// Inheritance
 
 // It is basically concept of inherit properties from Parent clas to Child class.
-// In real world, when Child gets property or something from Parent that is concept of inheritance.
-//
+// In real world, when Child gets property or something from Parent that tha concept of inheritance.
+// In simple words,     It is sharing the behaviour and properties between two classes.
+// It allow to define class that extend functionalities from another clas.
+// ' extend ' keyword is use to inherit properties from parent class.
+
 
 
 
