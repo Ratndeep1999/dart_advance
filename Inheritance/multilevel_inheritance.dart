0ff70efe4e70de9173cccc13032b1cc8509497af
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-24 17:56:37 +0530

    multilevel_inheritance.dart : multilevel inheritance

--- Inheritance/multilevel_inheritance.dart
+++ Inheritance/multilevel_inheritance.dart
@@ -3,3 +3,5 @@
 
 
 /// Multilevel Inheritance
+// A clas can inherit from another class and that class also can inherit from another class .
+// we can extend a class from another class which is already extended from another class .
