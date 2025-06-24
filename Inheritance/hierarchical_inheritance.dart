c8ec0b5aba0d2d53f14d91b4900c94fd87556fd7
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-24 18:58:46 +0530

    hierarchical_inheritance.dart : hierarchical inheritance,

--- Inheritance/hierarchical_inheritance.dart
+++ Inheritance/hierarchical_inheritance.dart
@@ -1,4 +1,9 @@
 
 
 
-/// Hierarchical Inheritance
+/// Hierarchical Inheritance :
+// Here, Parent class is Inherited by multiple classes.
+// In simple term, Hierarchical inheritance occur when multiple classes inherit from single Parent class.
+// e.g  Tesla, Honda, Tata  inherited from Car class
+
+
