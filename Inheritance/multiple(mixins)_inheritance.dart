160235d1e93a2c8555e9c7ee4317250f7751fe67
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-25 10:20:39 +0530

    multiple(mixins)_inheritance.dart : multiple inheritance, e.g of multiple inheritance

--- Inheritance/multiple(mixins)_inheritance.dart
+++ Inheritance/multiple(mixins)_inheritance.dart
@@ -2,3 +2,8 @@
 
 
 /// Multiple Inheritance :
+// Here Dart does not support Multiple Inheritance
+// but, we can achieve this phenomenon using mixins
+// Multiple Inheritance is basically the ability of class to inherit from more than one parent class
+
+// e.g of Multiple Inheritance ( Mixins )
