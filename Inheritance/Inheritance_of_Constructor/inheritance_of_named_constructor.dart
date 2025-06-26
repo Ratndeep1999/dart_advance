bdc0d6180bb4196b7cb77c19a9708be8f38e3e7e
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-26 14:03:49 +0530

    inheritance_of_constructor_with_named_parameters_with_optional.dart : e.g of inheritance_of_constructor_with_named_parameters_with_optional

---
+++ Inheritance/Inheritance_of_Constructor/inheritance_of_named_constructor.dart
@@ -0,0 +1,17 @@
+
+
+// Inheritance of Named constructor
+
+class Parent {
+
+  // normal constructor
+  Parent() {
+    print('Parent constructor');
+  }
+
+  // named constructor
+  Parent.named () {
+    print("Parent Named constructor");
+  }
+}
+
