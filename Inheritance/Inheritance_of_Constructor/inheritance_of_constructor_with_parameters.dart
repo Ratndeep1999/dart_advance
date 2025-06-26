c9864b731420f4b07564f3ebbf51692e863fda5e
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-26 12:19:00 +0530

    inheritance_of_constructor_with_parameters.dart : e.g of inheritance_of_constructor_with_parameters

--- Inheritance/Inheritance_of_Constructor/inheritance_of_constructor_with_parameters.dart
+++ Inheritance/Inheritance_of_Constructor/inheritance_of_constructor_with_parameters.dart
@@ -8,7 +8,7 @@ class Person {
 
   // constructor with parameters
   Person (String name, int age) {
-    print('Parent constructor');
+    // print('Parent constructor');
     print('Name : $name');
     print('Age : $age');
   }
@@ -19,7 +19,14 @@ class Employee extends Person {
 
   // constructor of employee
   Employee (double salary, String name, int age) : super(name, age) {
-    print('Child constructor');
+    // print('Child constructor');
     print('Salary : $salary');
   }
+}
+
+
+void main() {
+
+  Employee employee = Employee(23000, 'Sonali', 23);
+
 }
