d487634c393fa10142c81f4d779cf3ef0956621d
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-26 12:03:27 +0530

    inheritance_of_constructor.dart : constructor, note, e.g of constructor

--- Inheritance/Inheritance_of_Constructor/inheritance_of_constructor.dart
+++ Inheritance/Inheritance_of_Constructor/inheritance_of_constructor.dart
@@ -3,3 +3,14 @@
 /// Inheritance of Constructor
 
 // It is the process of inheriting of constructor from parent class to child class
+
+// [ Note : First Parent class constructor call then child class constructor call when we create object ]
+
+// e.g of constructor calling without parameters
+class Parent {
+
+  // constructor
+  Parent() {
+    print('This is parent class constructor');
+  }
+}
