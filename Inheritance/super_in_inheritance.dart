b29f6fa9c6ada244127527096a075ba782626f4e
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-26 22:23:43 +0530

    super_in_inheritance.dart : super, e.g of super with extra

--- Inheritance/super_in_inheritance.dart
+++ Inheritance/super_in_inheritance.dart
@@ -5,3 +5,14 @@
 // before learn ' Super ', need to understand constructor and inheritance
 
 // Super keyword is use to refer to Parent class, It is use to call Parent class properties and methods
+
+// e.g of super keyword with properties and methods
+
+class Continent {
+  String name ;
+  double landmass ;
+  int landmassRank ;
+
+  Continent (this.name, this.landmass, this.landmassRank);
+
+}
