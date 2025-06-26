9345a3241e68af5ac9362279904bc43f9a2a20d3
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-26 13:26:47 +0530

    inheritance_of_constructor_with_named_parameters_with_optional.dart : e.g of inheritance_of_constructor_with_named_parameters_with_optional

--- Inheritance/Inheritance_of_Constructor/inheritance_of_constructor_with_named_parameters_with_optional.dart
+++ Inheritance/Inheritance_of_Constructor/inheritance_of_constructor_with_named_parameters_with_optional.dart
@@ -7,7 +7,18 @@ class Person {
   int? age ;
   String? breed ;
 
-  // constructor with named parameters
+  // constructor with named parameters with optional
   Person ({ this.name, this.age, this.breed = 'Humans (Homo sapiens) or modern humans' });
 
+}
+
+class Partner extends Person {
+  String gender ;
+  String type ;
+
+  // constructor
+  Partner (this.gender, [this.type = 'Wife', String? name, int? age, String? breed]) : super( name: name, age: age, breed: breed ) ;
+
+  // Alternate approach
+  // Partner ({ this.gender, this.type = 'Wife', String? name, int? age, String? breed}) : super( name: name, age: age, breed: breed ) ;
 }
