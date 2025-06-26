a3e782a071319154eb29634e5e5447befe3cb71a
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-26 13:03:25 +0530

    inheritance_of_constructor_with_named_parameters_with_optional.dart : e.g of inheritance_of_constructor_with_named_parameters_with_optional

---
+++ Inheritance/Inheritance_of_Constructor/inheritance_of_constructor_with_named_parameters_with_optional.dart
@@ -0,0 +1,13 @@
+
+
+// Inheritance of constructor with Name Parameters
+
+class Person {
+  String? name ;
+  int? age ;
+  String? breed ;
+
+  // constructor with named parameters
+  Person ({ this.name, this.age, this.breed = 'Humans (Homo sapiens) or modern humans' });
+
+}
