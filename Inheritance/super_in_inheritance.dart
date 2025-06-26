8bc7a08a0bd54f078fdaa411efdae55d949d9518
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-26 22:13:38 +0530

    super_in_inheritance.dart : super,

---
+++ Inheritance/super_in_inheritance.dart
@@ -0,0 +1,7 @@
+
+
+/// Super keyword in Inheritance
+
+// before learn ' Super ', need to understand constructor and inheritance
+
+// Super keyword is use to refer to Parent class, It is use to call Parent class properties and methods
