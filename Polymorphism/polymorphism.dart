6f3279160f737d56f1adf338857f21b5880c3e25
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-29 19:38:24 +0530

    polymorphism.dart : polymorphism

--- Polymorphism/polymorphism.dart
+++ Polymorphism/polymorphism.dart
@@ -5,3 +5,4 @@
 // before polymorphism need to understanding of Inheritance.
 
 // poly = many, morph = forms   means 'ManyForms' - 'MultipleForms'
+// As Humans, we have abilities to take many forms Like, We can be Student, Labour, Teacher, Parent, Friend
