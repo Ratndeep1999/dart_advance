d919beb5c3d2d165e63616ede3ac55f173c6057b
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-29 19:40:55 +0530

    polymorphism.dart : polymorphism

--- Polymorphism/polymorphism.dart
+++ Polymorphism/polymorphism.dart
@@ -6,3 +6,6 @@
 
 // poly = many, morph = forms   means 'ManyForms' - 'MultipleForms'
 // As Humans, we have abilities to take many forms Like, We can be Student, Labour, Teacher, Parent, Friend
+
+// It is the core concept of OOp's Programing
+// Polymorphism allow to " Objects of different classes to be treated as object of common superclass. "
