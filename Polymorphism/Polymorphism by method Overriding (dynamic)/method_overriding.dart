25c2feb36d5ee5156aa7984afc6b18a94b19eb50
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-29 20:08:41 +0530

    method_overriding.dart :

--- Polymorphism/Polymorphism by method Overriding (dynamic)/method_overriding.dart
+++ Polymorphism/Polymorphism by method Overriding (dynamic)/method_overriding.dart
@@ -2,4 +2,4 @@
 
 
 // It is a technique in which,
-// I can create a method in child class that has the same name as the method in parent class
+// I can create a method in child class that has the same name as a method in parent class.
