5f0be1d856f29ffae1664b905cb1bc7a82399053
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-30 17:28:28 +0530

    enum.dart : enums, key points of enums, syntax

--- Enums/enum.dart
+++ Enums/enum.dart
@@ -10,3 +10,10 @@
 /// key points of enum :
 // each enum values is an instance of enum class and associated index (zero based)
 // it is immutable and cannot be instantiate beyond their defined values
+
+// Syntax :
+enum enumName {
+  constantName1,   // index value 0
+  constantName2,   // index value 1
+  constantName3    // index value 2
+}
