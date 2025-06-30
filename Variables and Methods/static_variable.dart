b240ef43fffab68a13189cf5c69a1aedcc7e8f57
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-30 13:38:53 +0530

    static_variable.dart : static_variable

--- Variables and Methods/static_variable.dart
+++ Variables and Methods/static_variable.dart
@@ -4,6 +4,7 @@
 /// Static Variable :
 
 
-// It is a class level variable declare with static keyword
-// It is a variable that is shared by all instance of clas
-//
+// It is a class level variable declare with static keyword.
+// It is a variable that is shared by all instance of clas.
+// it is shared across all instance of class,
+// means there is only one copy of variable regardless of how many objects are created.
