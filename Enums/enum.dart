765061350fbad6b24d1cf4c423ed007bc9d0c0b9
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-30 17:26:02 +0530

    enum.dart : enums, key points of enums

--- Enums/enum.dart
+++ Enums/enum.dart
@@ -1,8 +1,12 @@
 
 
 
-// Enumeration
+/// Enumeration
 
 // it is a special type to represent fixed number of data.
 // Enum represents fixed number of constant values.
-// enum declared using the keyword "enum"
+// enum declared using the keyword "enum".
+
+/// key points of enum :
+// each enum values is an instance of enum class and associated index (zero based)
+// it is immutable and cannot be instantiate beyond their defined values
