be43285258b067a2985803f9a37f910f58c21479
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-30 13:14:18 +0530

    instance_method.dart : instance method, characteristics, e.g of instance method

--- Variables and Methods/instance_method.dart
+++ Variables and Methods/instance_method.dart
@@ -11,3 +11,17 @@
 // define inside a class and can use this keyword to refer to the current instance
 // called on an object of class, not the class itself
 // Use to define behavior or functionality specific to an object
+
+// e.g
+class Person {
+  String name ;    // instance variable
+  int age ;   // instance variable
+
+  Person(this.name, this.age);    // constructor
+
+  // instance method
+  void intro() {
+    print('My name is $name and I am $age years old');
+  }
+}
+
