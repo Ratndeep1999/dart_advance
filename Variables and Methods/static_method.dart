b076762ba7e0545afb10d634441d32fcc149e092
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-30 16:14:04 +0530

    static_method.dart : static method,

--- Variables and Methods/static_method.dart
+++ Variables and Methods/static_method.dart
@@ -5,4 +5,5 @@
 
 // Static method is a class level method, declare with the static keyword
 // I can access static method without create object
-//
+// use for utility functions or operations related to the class as whole
+// called using ClassName.method()
