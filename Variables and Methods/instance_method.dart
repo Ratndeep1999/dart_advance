1e2c7d2784afcbde88bebc598b3ae9e8315a58dc
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-30 13:09:17 +0530

    instance_method.dart : instance method, characteristics,

--- Variables and Methods/instance_method.dart
+++ Variables and Methods/instance_method.dart
@@ -1,7 +1,13 @@
 
 
 
-/// Instance Method
+/// Instance Method :
 
 // Instance method is a non-static method defined within class that operates on an instance (object) of class
 // it can access instance variable and other instance method of the same object
+
+
+/// Characteristics :
+// define inside a class and can use this keyword to refer to the current instance
+// called on an object of class, not the class itself
+// Use to define behavior or functionality specific to an object
