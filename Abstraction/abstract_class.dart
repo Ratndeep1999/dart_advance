605f38e43f6d6f8175d1f7f8a21045339513742f
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-01 22:18:49 +0530

    abstract_class.dart : learn before abstract class, Concrete class, Abstract class

--- Abstraction/abstract_class.dart
+++ Abstraction/abstract_class.dart
@@ -7,7 +7,10 @@
 
 /// Concrete Class :
 // Class which objects is creatable.
+// We can create object of Concrete class.
 
-
+// Abstract Class :
+// Class which objects is not creatable.
+// We can not create object of abstract class
 
 
