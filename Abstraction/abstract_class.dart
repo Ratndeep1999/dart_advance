d9fb0dd09cd22b35d931e53e46ae5c9b3d0d8c64
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-01 22:25:22 +0530

    abstract_class.dart : learn before abstract class, Concrete class, Abstract class,

--- Abstraction/abstract_class.dart
+++ Abstraction/abstract_class.dart
@@ -5,12 +5,20 @@
 
 // before abstract class learn constructor and inheritance
 
+
+/// Difference between Concrete class and Abstract class
+
 /// Concrete Class :
 // Class which objects is creatable.
 // We can create object of Concrete class.
 
-// Abstract Class :
+/// Abstract Class :
 // Class which objects is not creatable.
 // We can not create object of abstract class
 
 
+/// Abstract Class :
+// Class that can not be initialized.
+// declare class using " Abstract " keyword.
+// Use to define behavior/method of class that can be inherited by other classes.
+// It focus on hiding complex implementation logic of system and expose only needable coe logic.
