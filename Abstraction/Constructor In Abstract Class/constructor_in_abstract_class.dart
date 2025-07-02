c8adc5d030ead76aa421dadb2c2ba65fed6455f8
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-02 14:17:22 +0530

    constructor_in_abstract_class.dart : constructor_in_abstract_class,

---
+++ Abstraction/Constructor In Abstract Class/constructor_in_abstract_class.dart
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+/// Constructor in Abstract class
+
+
+// unable to create object of abstract class but
+// able to define constructor in abstract class
+// Abstract class constructor call when subclass constructor calls
