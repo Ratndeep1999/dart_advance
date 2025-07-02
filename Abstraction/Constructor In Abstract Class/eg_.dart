801dc3c5b242d1eb82a962f2e69d8e25d00e4c91
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-02 14:25:29 +0530

    eg_.dart : abstract class person with properties, constructor and methods intro() and qualification()

--- Abstraction/Constructor In Abstract Class/eg_.dart
+++ Abstraction/Constructor In Abstract Class/eg_.dart
@@ -7,9 +7,11 @@ abstract class Person {
 
   Person(this.name, this.age);
 
+  // concrete method
   void intro() {
     print('My Name is $name and I am $age years old');
   }
 
+  // abstract method
   void qualification();
 }
