e8c2f3f7008b062deb1bf3e77193fc1a65880674
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-02 15:09:55 +0530

    interface.dart : interface,

--- Abstraction/Interface/interface.dart
+++ Abstraction/Interface/interface.dart
@@ -4,4 +4,9 @@
 
 /// Interface
 
+// use to achieve abstraction in
+// implement keyword is must to use implement
+// use interface for class that implement interface
+// must to implement all methods and properties in interface
+
 
