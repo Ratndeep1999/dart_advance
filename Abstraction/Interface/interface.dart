f40272f59adbb1e59d1f5e4343dff91b464be857
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-02 19:05:31 +0530

    interface.dart : interface, extend, implement

--- Abstraction/Interface/interface.dart
+++ Abstraction/Interface/interface.dart
@@ -6,6 +6,15 @@
 
 // Interface provide blueprints
 // little bit part similar to abstract class
-// We can use 'extend' or 'implement' keyword, both have different work in interface
+// here, must to define body of method
+// interface allow to create object
 //
-//
+
+/// 'extend' and 'implement' both behave differently in case of interface
+
+/// extend :
+// polymorphism not work here
+// I can not extend interface in another file
+// If class extend then optional to @override method
+// means, when we @override any method then we are adding extra functionality to that method
+
