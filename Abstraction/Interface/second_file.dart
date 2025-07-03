1d1488cda4cb75a7ea4b75e908de6489f8f86e8e
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-03 16:10:29 +0530

    second_file.dart : e.g of file handling, i am accessing interface using extend,

--- Abstraction/Interface/second_file.dart
+++ Abstraction/Interface/second_file.dart
@@ -5,6 +5,7 @@
 import 'eg_of_interface.dart';
 
 // class Ship
-// class Ship extends Vehicle { }
+// class Ship extends Vehicle { } .. this gives compile time error because
+// [ Note : we cant access Interface using extend keyword in another file ]
 
 
