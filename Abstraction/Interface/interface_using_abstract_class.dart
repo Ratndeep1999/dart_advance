34871dff9f3ef61056bd0b9bc1bc35061921f23c
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-03 14:24:16 +0530

    interface_using_abstract_class.dart : abstract clas Bird with abstract method canFly() and canSwim(),

--- Abstraction/Interface/interface_using_abstract_class.dart
+++ Abstraction/Interface/interface_using_abstract_class.dart
@@ -7,7 +7,14 @@ abstract class Bird {
   String? name ;
   static String breed = 'Bird' ;
 
+  // abstract method
+  void canFly();
+  void canSwim();
 
+  // concrete method
+  // static void type(){  // I don't want to implement this method in child classes, if i want then i can make it static and call using super
+  //   print('$name is breed of $breed');  // but here name is not static so i need to remove name or make it static
+  // }
 
 }
 
