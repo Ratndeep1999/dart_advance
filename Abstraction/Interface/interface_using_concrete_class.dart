90bcb52977f930a7861fe139871fb2270237013f
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-04 12:00:45 +0530

    interface_using_concrete_class.dart : concrete class fan which implement by TableFan class with methods turnOn(), turnOff(), low(), mid(), and high(),

--- Abstraction/Interface/interface_using_concrete_class.dart
+++ Abstraction/Interface/interface_using_concrete_class.dart
@@ -13,3 +13,30 @@ class Fan {
   void high() {}
 }
 
+class TableFan implements Fan {
+  @override
+  void high() {
+    // TODO: implement high
+  }
+
+  @override
+  void low() {
+    // TODO: implement low
+  }
+
+  @override
+  void mid() {
+    // TODO: implement mid
+  }
+
+  @override
+  void turnOff() {
+    // TODO: implement turnOff
+  }
+
+  @override
+  void turnOn() {
+    // TODO: implement turnOn
+  }
+
+}
