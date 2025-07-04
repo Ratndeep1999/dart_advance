eeea44cb778827f2ba4441287a36e086b064dd49
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-04 12:14:40 +0530

    interface_using_multiple_inheritance.dart : multiple inheritance, multiple inheritance using interface

--- Abstraction/Interface/interface_using_multiple_inheritance.dart
+++ Abstraction/Interface/interface_using_multiple_inheritance.dart
@@ -2,7 +2,7 @@
 
 /// Multiple Inheritance : Inherit from more than one class
 
-// Multiple inheritance using interface : Inherit from more than one interface class
+/// Multiple inheritance using interface : Inherit from more than one interface class
 
 
 // e.g of interface using multiple inheritance like.. mixins
