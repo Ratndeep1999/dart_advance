cb4d4a632e4e1b00020ac10263e5105f6230ab37
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-05 09:12:42 +0530

    mixin.dart : mixin, key points to remember,

--- Mixins/mixin.dart
+++ Mixins/mixin.dart
@@ -4,5 +4,7 @@
 /// Mixin : I tis use to inherit from specific and multiple parent classes to reuse the code
 
 /// Key points to remember :
-
+// 'mixin' keyword is use to declare Mixin before mixin name
+// mixin term work with this three keywords : mixin, with, on
+// multiple mixin can be use in single class
 
