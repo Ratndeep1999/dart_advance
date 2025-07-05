818282bf0180475ce5fc515f4937555036a87e43
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-05 09:07:41 +0530

    mixin.dart : mixin,

--- Mixins/mixin.dart
+++ Mixins/mixin.dart
@@ -1,1 +1,8 @@
 
+// before learn mixin learn constructor and inheritance
+
+/// Mixin : I tis use to inherit from specific and multiple parent classes to reuse the code
+
+/// Key points to remember :
+
+
