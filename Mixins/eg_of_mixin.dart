382d98359abf3a9d1e924850147751ba68cf6ee9
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-05 10:01:20 +0530

    eg_of_mixin.dart : mixin CanFly, mixin CanSwim, class KingFisher with CanFly and CanSwim, method added, class Swan with CanFly and CanSwim,

--- Mixins/eg_of_mixin.dart
+++ Mixins/eg_of_mixin.dart
@@ -11,7 +11,23 @@ mixin CanSwim {
   void canSwim();
 }
 
+// class KingFisher with mixin CanFly and CanSwim
 class KingFisher with CanFly, CanSwim {
+  String? name ;
+
+  @override
+  void canFly() {
+    print('$name can Fly.');
+  }
+
+  @override
+  void canSwim() {
+    print('$name can Swim');
+  }
+}
+
+// class Swan with mixin CanFly and CanSwim
+class Swan with CanFly, CanSwim {
   String? name ;
 
   @override
