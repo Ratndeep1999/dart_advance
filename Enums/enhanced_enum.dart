a7cb980267533cfbafe2839cd03b7facb470df33
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-09 11:59:02 +0530

    enhanced_enum.dart : syntax,

--- Enums/enhanced_enum.dart
+++ Enums/enhanced_enum.dart
@@ -1,4 +1,25 @@
 
 
 
-// this is enhanced enum which is advance part of enums
+// this is enhanced enum which is advance part of enums.
+
+/// Syntax
+
+// enum EnumName {
+//   // FieldsName
+//   value1('parameters'),
+//   value2('parameters'),
+//   value3('parameters');
+//
+//   // variable must be final
+//   final type variableName ;
+//
+//   // constructor must be constant
+//   const EnumName(this.variableName);
+//
+//   // method
+//   returnType methodName() {
+//     return statement ;
+//   }
+//
+// }
