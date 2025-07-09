506ff7ffbcf185bdce22831d1a0557b72c9a7b3b
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-09 17:15:18 +0530

    eg_of_null.dart : Person class, Female class which inherit members from class Person

--- Null Safety/eg_of_null.dart
+++ Null Safety/eg_of_null.dart
@@ -1,3 +1,29 @@
 
 
 //  e.g  to assign default values if value is null
+
+
+import 'dart:vmservice_io';
+
+class Person {
+  String? name ;
+  dynamic hobbies ;
+
+  Person(this.name, this.hobbies);
+
+  void details(){}
+}
+
+class Female extends Person {
+  String? gender ;
+
+  Female(this.gender,String? name, dynamic hobbies) : super(name, hobbies);
+
+  void details() {
+    print('Name : ${name = name ?? 'Unknown'}');
+    print('Hobbies : ${hobbies = hobbies ?? 'Not Given'}');
+    print('Gender : ${gender = gender ?? 'Others'}');
+  }
+}
+
+
