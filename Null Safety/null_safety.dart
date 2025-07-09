6a5c9d12896dca38c1ef4aab3ea3ec8c9f1e41d7
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-09 15:34:54 +0530

    null_safety.dart : null safety,

---
+++ Null Safety/null_safety.dart
@@ -0,0 +1,10 @@
+
+
+
+/// Null Safety :
+// It helps to avoid null errors in dart programing language.
+// We define null using '?' symbol after type declaration.
+// It helps to find / catch null errors at Compile-Time
+// This feature is known as sound null check.
+
+
