0389c4800895a78fdbebabb12124dd79e0a1da0c
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-09 17:19:53 +0530

    eg_of_null.dart : Person class, Female class which inherit members from class Person, objects of Female class

--- Null Safety/eg_of_null.dart
+++ Null Safety/eg_of_null.dart
@@ -22,8 +22,24 @@ class Female extends Person {
   void details() {
     print('Name : ${name = name ?? 'Unknown'}');
     print('Hobbies : ${hobbies = hobbies ?? 'Not Given'}');
-    print('Gender : ${gender = gender ?? 'Others'}');
+    print('Gender : ${gender = gender ?? 'Others'}\n');
   }
 }
 
 
+void main() {
+
+  // object of sonali
+  Female sonali = Female('Female', 'Sonali', null);
+  sonali.details();
+
+  // object of female1
+  Female female1 = Female(null, null, 'Singing, Dancing, Smiling');
+  female1.details();
+
+  // object of female2
+  Female female2 = Female(null, null, null);
+  female2.details();
+
+
+}
