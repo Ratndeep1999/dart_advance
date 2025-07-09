9855aae146549f26fb066b71fcf2e5f38933ea32
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-09 13:06:25 +0530

    factory_constructor.dart : generative constructor,

--- Constructors/Factory Constructor/factory_constructor.dart
+++ Constructors/Factory Constructor/factory_constructor.dart
@@ -9,3 +9,7 @@
 // previously we learn generative / normal constructor .
 // It create an instance of class .
 // can't return instance of class or subclass .
+
+// Factory constructor :
+// The constructor which return cached instance of class.
+// it allow to give validation to constructor when we create it
