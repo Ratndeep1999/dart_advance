a87ef3f54323ff9dba828e5d0c3182c681469af7
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-09 17:34:40 +0530

    late.dart : definition, Note,

--- Late keyword/late.dart
+++ Late keyword/late.dart
@@ -5,4 +5,7 @@
 // It ensures that the variable must be initialized before program run's other wise it throw exception
 
 // It allow to define nonnullable variable at the time of define
-// but must be initialise before run
+// but must be initialise before run.
+
+// [ Note : It tell compiler that developer don't have any value
+//  right now but it must be use before program run ]
