2ff0003989f02cb2338e0ac45ff8a169b22433c6
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-10 14:23:56 +0530

    type_promotion.dart : type promotion, Types, example

--- Type Promotion/type_promotion.dart
+++ Type Promotion/type_promotion.dart
@@ -10,3 +10,35 @@
 
 // 1. General type to specific types
 // 2. Nullable types to non-nullable types
+
+
+class Person {
+  final String? name ;
+  final Address? address ;
+
+  Person({this.name, this.address});
+
+}
+
+class Address {
+  final String? city ;
+  final String? state ;
+  final String? street ;
+
+  Address({this.city, this.state, this.street});
+}
+
+
+void main() {
+
+  Person person = Person(name: 'Ratndeep', address: Address(city: 'Wardha'));
+
+  // If i am using this kind of format then it is not good
+  if (person.address != null ) {
+    print('City : ${person.address!.city}');
+    print('State : ${person.address?.state}');
+    print('Street : ${person.address?.street}');
+  } else {
+    print('Data is null');
+  }
+}
