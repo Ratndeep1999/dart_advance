1becc080e1bece389a3967adf594adde1d4e0e97
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-10 14:53:19 +0530

    const_and_final.dart : const and final,

--- const_and_final.dart
+++ const_and_final.dart
@@ -5,8 +5,11 @@
 // Const :
 // 1. It is compile time constant value that never get change
 // 2. 2. DateTime.now(); is possible
+// 3.
+
 
 
 // Final :
 // 1. It is Run time constant
 // 2. DateTime.now(); is possible
+// 3. use when values fetch from the server or database.
