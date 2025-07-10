e2369c8ac094ac8ee29a1b112d7147c98593627a
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-10 14:04:46 +0530

    type_promotion.dart : type promotion, Types

--- Type Promotion/type_promotion.dart
+++ Type Promotion/type_promotion.dart
@@ -4,3 +4,9 @@
 /// Type Promotion
 
 // It allow to convert value of one type to another type , but it happens when value is of a specific type.
+
+
+/// Types
+
+// 1. General type to specific types
+// 2. Nullable types to non-nullable types
