3e561b256e14c1d6ff5147729e06e361e10231cc
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-10 15:04:08 +0530

    const_and_final.dart : const and final,

--- const_and_final.dart
+++ const_and_final.dart
@@ -1,17 +1,22 @@
 
 
 /// Const and Final
+// Both are immutable ( value that can not change )
 
-// Const :
+
+/// Const :
 // 1. It is compile time constant value that never get change
-// 2. 2. DateTime.now(); is possible
-// 3.
+// 2. perform  DateTime.now(); is not possible
+// 3. initialize at compile time
+// 4. cannot use runtime values
 
 
-
-// Final :
+/// Final :
 // 1. It is Run time constant
-// 2. DateTime.now(); is possible
-// 3. use when values fetch from the server or database.
-// 4. use when want to read the local files.
-// 5. use to get network request that can not be changed.
+// 2. perform DateTime.now(); is possible
+// 3. initialize at run time
+// 4. can be assign dynamically at runtime [ user input, API response ]
+
+
+
+// use :  values fetch from the server or database, want to read the local files, to get network request that can not be changed
