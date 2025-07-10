58bba980947ae3d4077bd376eafb1c585ed0f4aa
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-10 14:55:00 +0530

    const_and_final.dart : const and final,

--- const_and_final.dart
+++ const_and_final.dart
@@ -13,3 +13,5 @@
 // 1. It is Run time constant
 // 2. DateTime.now(); is possible
 // 3. use when values fetch from the server or database.
+// 4. use when want to read the local files.
+// 5. use to get network request that can not be changed.
