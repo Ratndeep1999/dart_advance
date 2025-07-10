99eca774f63aa7ae37398237b2931f59535db53f
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-10 19:15:28 +0530

    write.dart : write, method, eg of write to write content inside file through writeAsStringSync() method

--- File Handling/write.dart
+++ File Handling/write.dart
@@ -1,8 +1,24 @@
 
 
 
+import 'dart:io';
+
 /// Write :
 // this allow to write content inside file.
 
 // Method :
-// writeAsStringSync() : use to write inside file.
+// writeAsStringSync() : use to write inside file / replace previous data.
+
+
+
+void main() {
+
+  // write content inside file
+  File writeFile = File('write.txt');
+  // use this method to write inside the given file
+  writeFile.writeAsString('Hi, I am Ratndeep\nI love to Read, Trade, and Travelling');
+  // print content if write again then update it
+  print('Content : ${writeFile.readAsStringSync()}');
+
+}
+
