c26db166ab44a415e16e765925951034834f32fe
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-11 12:43:31 +0530

    exception_handling.dart : exception handling,

---
+++ Exception Handling/exception_handling.dart
@@ -0,0 +1,10 @@
+
+
+/// Exception Handling
+
+
+// It's an error that occurs at runtime during program execution.
+// If exception trigger the flow of program will stop and, program behave like crashed.
+// It is good habit to handle the exception to save program from crashing.
+
+
