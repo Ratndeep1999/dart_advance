f4678aeba076430fe2594e65f57c730c45058768
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-11 17:13:27 +0530

    on_keyword.dart : on keyword, eg of On keyword to print exception that already know

--- Exception Handling/on_keyword.dart
+++ Exception Handling/on_keyword.dart
@@ -4,3 +4,25 @@
 /// On :
 
 // if we know what exception will occur at that time we use on keyword.
+
+
+
+void main() {
+
+  int a = 10 ;
+  int b = 0 ;
+
+  try {
+    print(a ~/ b);
+  }
+  on UnsupportedError {     // on use to print exception
+    print('Cannot divide by zero');
+  }
+  catch (e) {
+    print(e);
+  }
+  finally {
+    print("Finally block execute at last an any condition");
+  }
+
+}
