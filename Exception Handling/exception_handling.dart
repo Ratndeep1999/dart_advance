ae644c16ad3d352f400396b5ff4d2c392e7acbeb
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-11 13:08:51 +0530

    exception_handling.dart : exception handling, try, catch and final, Syntax,

--- Exception Handling/exception_handling.dart
+++ Exception Handling/exception_handling.dart
@@ -8,6 +8,16 @@
 // To save the program from crashing we need to catch the exception.
 
 
+/// try, catch and final :
+
+// try : within ' try ' body we can write the logical code that execute at runtime if exception occur then it throw to ' catch ' body.
+
+// catch : 'catch block' written with 'try' to catch the exception throw by try, we don't know what kind of exception occurs
+//          we just use catch to print the exception in terminal and it makes program safer from crashing
+
+// final : this block always executes whether exception trigger or not this block of code always executes at last and it is optional to use.
+
+
 /// Syntax :
 
 // try {
@@ -15,3 +25,8 @@
 // } catch (ex) {
 //   // exception
 // }
+// finally {
+//   // code that always executes at last if exception occur or not
+// }
+
+
