d4e95358325a81160a5ac7c785a41b9200f9ad3a
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-11 17:04:54 +0530

    throw_keyword.dart : throw keyword, eg of throw keyword that throw exception

--- Exception Handling/throw_keyword.dart
+++ Exception Handling/throw_keyword.dart
@@ -2,3 +2,20 @@
 
 /// throw :
 // use when we have to give formated Exception.
+
+void main() {
+
+  try {
+    nameCheck('Sonali');
+  }
+  catch (e) {
+    print("Name is Not same");
+  }
+}
+
+
+void nameCheck(String name) {
+  if ( name != 'Ratndeep') {
+    throw FormatException();
+  }
+}
