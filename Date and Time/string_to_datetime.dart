71fc16ddfc6ec2328e444c139bf6afde4785a3b9
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-11 18:09:45 +0530

    string_to_datetime.dart : description to convert String to DateTme,

--- Date and Time/string_to_datetime.dart
+++ Date and Time/string_to_datetime.dart
@@ -3,3 +3,14 @@
 // Here, I Convert my Date and Time String type to DateTime type
 // i can not use directly String as a type of DateTime type
 // mostly .parse is use to convert one type to another type
+
+
+void main() {
+
+  String myTimelineInString = "1999-02-06" ;   // store my string timeline to DateTime
+  DateTime myTimelineInDateTime = DateTime.parse(myTimelineInString);   // converted string to DateTime type
+
+  // my Birthday
+
+
+}
