385e57cdecb5309de096efa0ed2a74ed383ec878
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-11 19:10:28 +0530

    comparison_methods.dart : methods

--- Date and Time/comparison_methods.dart
+++ Date and Time/comparison_methods.dart
@@ -2,3 +2,8 @@
 // Methods use to compare dates
 
 
+// isBefore(dateTime) : it returns bool values
+// isAfter(dateTime) : it returns bool values
+// isAtSameMovementAs(dateTime) : it returns bool values
+
+
